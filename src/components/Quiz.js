e53010816242862466a9170455b22f10e157bbe3
Alex Fotopoulos <alex.foto.dev@gmail.com>
2022-04-19 16:30:10 -0400

    implement API call to get new questions and solve encoding issue

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -8,9 +8,35 @@ export default function () {
     const [isGameOver, setIsGameOver] = useState(false)
     const quizCtx = useContext(QuizContext)
 
+    function b64DecodeUnicode(str) {
+        // Going backwards: from bytestream, to percent-encoding, to original string.
+        return decodeURIComponent(atob(str).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    }
+
     useEffect(() => {
-        quizCtx.addData(DummyData)
-        quizCtx.addCorrectAnswers(DummyData)
+        async function fetchData() {
+            let response = await fetch('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple&encode=base64')
+            let data = await response.json()
+            console.log(data.results)
+            for (let result of data.results) {
+                result.category = b64DecodeUnicode(result.category)
+                result.question = b64DecodeUnicode(result.question)
+                result.correct_answer = b64DecodeUnicode(result.correct_answer)
+                result.difficulty = b64DecodeUnicode(result.difficulty)
+                result.type = b64DecodeUnicode(result.type)
+                let choices = []
+                for (let answer of result.incorrect_answers) {
+                    choices.push(b64DecodeUnicode(answer))
+                }
+                choices.push(result.correct_answer)
+                result.choices = choices
+            }
+            quizCtx.addData(data.results)
+            quizCtx.addCorrectAnswers(data.results)
+        }
+        fetchData()
     }, [])
 
     function handleSubmit() {
