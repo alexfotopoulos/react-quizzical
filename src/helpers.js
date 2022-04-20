3cd462bc7e6ae6a866ca1bd0fc5c9d7794dd904d
Alex Fotopoulos <alex.foto.dev@gmail.com>
2022-04-20 16:08:29 -0400

    add loading spinner between start/reset and require all questions to be answered before submission

--- src/helpers.js
+++ src/helpers.js
@@ -22,24 +22,3 @@ export function b64DecodeUnicode(str) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
 }
-
-export async function fetchData() {
-    let response = await fetch('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple&encode=base64')
-    let data = await response.json()
-    console.log(data.results)
-    for (let result of data.results) {
-        result.category = b64DecodeUnicode(result.category)
-        result.question = b64DecodeUnicode(result.question)
-        result.correct_answer = b64DecodeUnicode(result.correct_answer)
-        result.difficulty = b64DecodeUnicode(result.difficulty)
-        result.type = b64DecodeUnicode(result.type)
-        let choices = []
-        for (let answer of result.incorrect_answers) {
-            choices.push(b64DecodeUnicode(answer))
-        }
-        choices.push(result.correct_answer)
-        shuffle(choices)
-        result.choices = choices
-    }
-    return data.results
-}
